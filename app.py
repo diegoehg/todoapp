7dae53ea4466f9d57ed81ba6aead9a6d3f5efc31
Diego Hdez <di_hernandez@uxmcc2.iimas.unam.mx>
2020-12-12 13:45:36 -0600

    Add Todo model

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://todoapp:todo_user@localhost:5432/todoapp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f"<Todo id:{self.id}, description:{self.description}>"
+
+
+db.create_all()
+
 
 @app.route("/")
 def index():
