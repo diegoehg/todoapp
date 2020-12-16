94ff53ef558aec818c38cacd43b1540ee836ac93
Diego Hdez <di_hernandez@uxmcc2.iimas.unam.mx>
2020-12-16 13:52:37 -0600

    Added links to todo lists and recovering their todo items

--- app.py
+++ app.py
@@ -38,8 +38,11 @@ def index():
 
 @app.route("/lists/<list_id>")
 def get_list_todos(list_id):
-    list_todos = Todo.query.filter_by(list_id=list_id).order_by('id').all()
-    return render_template('index.html', data=list_todos)
+    return render_template('index.html',
+            lists=TodoList.query.all(),
+            active_list=TodoList.query.get(list_id),
+            todos=Todo.query.filter_by(list_id=list_id).order_by('id').all()
+    )
 
 
 @app.route("/todos/create", methods=['POST'])
